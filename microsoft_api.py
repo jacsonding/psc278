ed20cbabf27368e6169f67ed6213dc585346d660
Ding <zding5@glsn-imac-22.ur.rochester.edu>
2018-05-06 03:49:30 -0400

    added function to p2 also. p3 has SSL error on my mac

--- microsoft_api.py
+++ microsoft_api.py
@@ -1,13 +1,24 @@
 import httplib, urllib, base64
 
-headers = {
-    # Request headers
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '8cb633cbd35c46899ccda26667370ba0',
-}
+def requestChunk(input):
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '8cb633cbd35c46899ccda26667370ba0',
+    }
 
-params = urllib.urlencode({
-})
+    params = urllib.urlencode({
+    })
+
+    try:
+        conn = httplib.HTTPSConnection('eastus.api.cognitive.microsoft.com')
+        conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, input, headers)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        return data
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 input = "{ \
  \"documents\": [ \
@@ -24,12 +35,5 @@ input = "{ \
   ]\
 }"
 
-try:
-    conn = httplib.HTTPSConnection('eastus.api.cognitive.microsoft.com')
-    conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, input, headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+a = requestChunk(input)
+print(a)
