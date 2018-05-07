65271f5075be8dcb1649d421c0f1c5812d508b15
Ding <zding5@rett-imac-01.ur.rochester.edu>
2018-05-07 17:25:45 -0400

    faster PlotGame (relative time)

--- PlotGame.m
+++ PlotGame.m
@@ -3,66 +3,51 @@
 % Note: NBA has LAC, LAL, BKN, NYK (do not unique that)
 
 % Cities/Teams
-cities = unique(x.subreddit);
+% cities = unique(x.subreddit);
+dayTime = 86400;
 
 % Want to find the nearest game AND the difference in time AND condition
 %% Create x table and new columns
 x = sortrows(reddit,2);
-ClosestGame = array2table(zeros(size(x,1),1),'VariableNames',{'ClosestGame'});
-RelativeTime = array2table(zeros(size(x,1),1),'VariableNames',{'RelativeTime'});
-Condition = cell2table(cell(size(x,1),1),'VariableNames',{'Condition'});
-x = [x ClosestGame RelativeTime Condition];
+ClosestGameW = zeros(size(x,1),1);
+RelativeTimeW = zeros(size(x,1),1);
+ClosestGameL = zeros(size(x,1),1);
+RelativeTimeL = zeros(size(x,1),1);
+
 
 
 %% For loop
-% Loop through the games
+% Loop through the COMMENTS
 for i=1:size(x,1)
-    thisCity = x.subreddit{i};
+    thisCity = x.City(i);
+    thisTime = x.created_utc(i);
+    startTime = thisTime-dayTime;
+    endTime = thisTime+dayTime;
     
-    currMin = realmax;
-    currGame = 0;
-    currCondition = 'null';
     
-    % Find eligible games
-    visitingL = strcmpi(NBA.VisitorNeutral,thisCity);
-    homeL = strcmpi(NBA.HomeNeutral,thisCity);
-    
-    allGames = find(visitingL | homeL);
-    visitingGames = find(visitingL);
-    homeGames = find(homeL);
-    
-    jindex = allGames;
-    for m=1:length(allGames)
-        j = jindex(m);
-        if xor(visitingL(j),homeL(j))
-            %Check to see if game is closer
-            if abs(NBA.UTC(j)-x.created_utc(i)) < abs(currMin)
-                currMin = x.created_utc(i)-NBA.UTC(j);
-                currGame = NBA.UTC(j);
-                
-                % if we win
-                if (visitingL(j) & (NBA.PTS(j) > NBA.PTS1(j)))
-                    currCondition = 'W';
-                else
-                    currCondition = 'L';
-                end
-                
-                if (homeL(j) & (NBA.PTS(j) > NBA.PTS1(j)))
-                    currCondition = 'L';
-                else
-                    currCondition = 'W';
-                end
-                
-            end
-        end
+    % Find wins
+    gameW = NBA((NBA.Winner == thisCity) & (NBA.UTC>startTime) & (NBA.UTC<endTime),:);
+    if ~isempty(gameW) %No games within the range
+        ClosestGameW(i) = gameW.UTC(1);
+        RelativeTimeW(i) = thisTime-gameW.UTC(1);
     end
     
-    % Add the information
-    x.ClosestGame(i) = currGame;
-    x.RelativeTime(i) = currMin;
-    x.Condition{i} = currCondition;
-   
+    % Find losses
+    gameL = NBA((NBA.Loser == thisCity) & (NBA.UTC>startTime) & (NBA.UTC<endTime),:);
+    if ~isempty(gameL) %No games within the range
+        ClosestGameL(i) = gameL.UTC(1);
+        RelativeTimeL(i) = thisTime-gameL.UTC(1);
+    end
+    
+    
+
     if mod(i,100000)==0
         disp(i)
     end
 end
+
+% ClosestGameW = array2table(ClosestGameW,'VariableNames',{'ClosestGameW'});
+% RelativeTimeW = array2table(RelativeTimeW,'VariableNames',{'RelativeTimeW'});
+% ClosestGameL = array2table(ClosestGameL,'VariableNames',{'ClosestGameL'});
+% RelativeTimeL = array2table(RelativeTimeL,'VariableNames',{'RelativeTimeL'});
+% x = [x ClosestGame RelativeTime Condition];
