0e19b7b1c07cc0ae8b9635fb3d129619940b9c74
Ding <zding5@rett-imac-01.ur.rochester.edu>
2018-05-07 15:52:39 -0400

    CODED the relative time twice but is still super slow

--- PlotGame.m
+++ PlotGame.m
@@ -5,6 +5,57 @@
 % Cities/Teams
 cities = unique(reddit.subreddit);
 
+% Want to find the nearest game AND the difference in time AND condition
+
 % Loop through the games
-for i=1:size(NBA,1)
+ClosestGame = [];
+RelativeTime = [];
+Condition = {};
+for i=1:size(reddit,1)
+    thisCity = reddit.subreddit{i};
     
+    currMin = realmax;
+    currGame = 0;
+    currCondition = 'null';
+    
+    % Find eligible games
+    visitingL = strcmpi(NBA.VisitorNeutral,thisCity);
+    homeL = strcmpi(NBA.HomeNeutral,thisCity);
+    
+    allGames = find(visitingL | homeL);
+    visitingGames = find(visitingL);
+    homeGames = find(homeL);
+    
+    for j=1:allGames      
+        if xor(visitingGames(j),homeGames(j))
+            %Check to see if game is closer
+            if abs(NBA.UTC(j)-reddit.created_utc(i)) < abs(currMin)
+                currMin = reddit.created_utc(i)-NBA.UTC(j);
+                currGame = NBA.UTC(j);
+                
+                % if we win
+                if (visitingGames(j) & (NBA.PTS(j) > NBA.PTS1(j)))
+                    currCondition = 'W';
+                else
+                    currCondition = 'L';
+                end
+                
+                if (homeGames(j) & (NBA.PTS(j) > NBA.PTS1(j)))
+                    currCondition = 'L';
+                else
+                    currCondition = 'W';
+                end
+                
+            end
+        end
+    end
+    
+    % Add the information
+    ClosestGame = [ClosestGame; currGame];
+    RelativeTime = [RelativeTime; currMin];
+    Condition = [Condition; currCondition];
+    
+    if mod(i,100000)==0
+        disp(i)
+    end
+end
--- merge_reddit_scores.m
+++ merge_reddit_scores.m
@@ -16,13 +16,27 @@
 % Score = table(zeros(size(reddit,1),1));
 % Error = table(cell(size(reddit,1),1));
 
+%% IMPORT FILES
+% reddit = readtable('2014_step1.csv');
+% errors = readtable('errors.csv');
+% scores = readtable('scores.csv');
+
+%% Merge
+
+scores.Properties.VariableNames{1} = 'id';
 ids = scores.id;ids = cellstr(ids);
 scores.id = ids;
 
 reddit = outerjoin(reddit,scores);
+
+reddit.Properties.VariableNames{6} = 'id';
+reddit(:,'id_scores') = [];
 
 errors.Properties.VariableNames{1} = 'id';
 ids = errors.id;ids = cellstr(ids);
 errors.id = ids;
 
 reddit = outerjoin(reddit,errors);
+
+reddit(:,'id_errors') = [];
+reddit.Properties.VariableNames{8} = 'Sentiment';
